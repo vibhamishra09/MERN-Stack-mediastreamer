aad9deeafea49354fcf8fbd57e1be7cc73852bd0
vibha mishra <vibhamishra@vibhas-MacBook-Air.local>
2025-11-04 00:33:42 +0530

    nehfefkikjdekjw

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, hydrate } from 'react-dom'
-import App from './App'
+import App from './App.js'
 
 const rootElement = document.getElementById('root')
 
---
+++ client/src/main.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { hydrate } from 'react-dom'
+import App from './App.js'
+
+hydrate(<App/>, document.getElementById('root'))
