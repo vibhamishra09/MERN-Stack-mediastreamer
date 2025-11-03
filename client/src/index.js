bd2f5006c8ecfae9dbdc6948addbac84c34afcbc
vibha mishra <vibhamishra@vibhas-MacBook-Air.local>
2025-11-04 00:27:13 +0530

    nehkikjdekjw

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render, hydrate } from 'react-dom'
+import App from './App'
+
+const rootElement = document.getElementById('root')
+
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<App/>, rootElement)
+} else if (rootElement) {
+  render(<App/>, rootElement)
+}
+
+
